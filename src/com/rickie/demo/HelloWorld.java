c61031c1db634b625b6970777c4465a9718bf8b2
rickiechina <rickiechina@hotmail.com>
2016-12-08 12:43:33 +0800

    Initial commit

---
+++ src/com/rickie/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.rickie.demo;
+
+/**
+ * Created by Rickie on 2016/12/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+    }
+}
